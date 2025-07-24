abdf181cda684584ffaa21c1a1afbb8028c88edf
N-node <drugnop@gmail.com>
2025-07-24 12:45:22 +0900

    baend update login is ok certification is ok upgrade version 7/24

---
+++ backend/src/main/java/com/example/backend/dto/UserResponseDto.java
@@ -0,0 +1,32 @@
+package com.example.backend.dto;
+
+import com.example.backend.entity.User;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class UserResponseDto {
+    private long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String userType;
+    private LocalDateTime createdAt;
+
+    public static UserResponseDto fromEntity(User user){
+        return UserResponseDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .phoneNumber(user.getPhoneNumber())
+                .userType(user.getPhoneNumber())
+                .userType(user.getUserType().name())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
+}
