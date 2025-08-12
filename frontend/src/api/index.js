6f7a433ae8fb3fa2217b11164ce782146891fff8
gurungd265 <gurungd265@gmail.com>
2025-08-12 17:39:13 +0900

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // VITE_API_BASE_URL=http://localhost:8080
-const API_BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+const API_BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'https://test8-1-p3pb.onrender.com';
 
 const api=axios.create({
     baseURL: API_BASE_URL,
